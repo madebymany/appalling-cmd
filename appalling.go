ced21ebefa8e265116ed7c905a140dbd82b3b0d9
Alex Barlow <alexbarlowis@gmail.com>
2013-10-15 10:28:51 +0100

    Support release notes and remove build no

--- appalling.go
+++ appalling.go
@@ -1,81 +1,81 @@
 package main
 
 import (
-  "bytes"
-  "fmt"
-  "io"
-  "mime/multipart"
-  "net/http"
-  "os"
-  "encoding/json"
-  "flag"
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
 )
 
 func main() {
-  var access_key string
-  var filepath   string
-  var build_no   string
-  var app_id     string
+	var access_key string
+	var app_id string
+	var filepath string
+	var release_notes string
 
-  flag.StringVar(&access_key, "key",    "",      "API key for upload/access")
-  flag.StringVar(&filepath,   "file",   "",      "Full path to file")
-  flag.StringVar(&build_no,   "build",  "0.0.1", "Build no")
-  flag.StringVar(&app_id,     "app_id", "0",     "App id")
+	flag.StringVar(&access_key, "key", "", "API key for upload/access")
+	flag.StringVar(&app_id, "app_id", "0", "App id")
+	flag.StringVar(&filepath, "file", "", "Full path to file")
+	flag.StringVar(&release_notes, "release_notes", "", "Full path to file")
 
-  flag.Parse()
+	flag.Parse()
 
-	target_url := fmt.Sprintf("http://apps.madebymany.com/admin/apps/%s/versions?auth_token=%s", app_id, access_key)
+	target_url := fmt.Sprintf("http://127.0.0.1:3000/admin/apps/%s/versions?auth_token=%s", app_id, access_key)
 	filename := filepath
-  extraParams := map[string]string{
-    "build_no" : build_no,
-  }
+	extraParams := map[string]string{
+		"release_notes": release_notes,
+	}
 
 	postFile(target_url, filename, extraParams)
 }
 
-func postFile(target_url string, filename string, params map[string]string){
-  // Turn our params map into JSON
-  json_bytes, err := json.Marshal(params)
-  if err != nil {
-    panic(err.Error())
-  }
-  json_str := string(json_bytes)
+func postFile(target_url string, filename string, params map[string]string) {
+	// Turn our params map into JSON
+	json_bytes, err := json.Marshal(params)
+	if err != nil {
+		panic(err.Error())
+	}
+	json_str := string(json_bytes)
 
-  // Create a new buffer for the body
-  body_buf := bytes.NewBufferString("")
-  body_writer := multipart.NewWriter(body_buf)
+	// Create a new buffer for the body
+	body_buf := bytes.NewBufferString("")
+	body_writer := multipart.NewWriter(body_buf)
 
-  file_writer, err := body_writer.CreateFormFile("file", filename)
-  if err != nil {
-          panic(err.Error())
-  }
+	file_writer, err := body_writer.CreateFormFile("file", filename)
+	if err != nil {
+		panic(err.Error())
+	}
 
-  fh, err := os.Open(filename)
-  if err != nil {
-      panic("Cannot find file")
-      panic(err.Error())
-  }
-  io.Copy(file_writer, fh)
+	fh, err := os.Open(filename)
+	if err != nil {
+		panic("Cannot find file")
+		panic(err.Error())
+	}
+	io.Copy(file_writer, fh)
 
-  content_type := body_writer.FormDataContentType()
+	content_type := body_writer.FormDataContentType()
 
-  body_writer.WriteField("data", json_str)
+	body_writer.WriteField("data", json_str)
 
-  body_writer.Close()
+	body_writer.Close()
 
-  fmt.Println("Uploading IPA..")
-  resp, err := http.Post(target_url, content_type, body_buf)
-  if err != nil {
-    panic(err.Error())
-  }
-  defer resp.Body.Close()
+	fmt.Println("Uploading App..")
+	resp, err := http.Post(target_url, content_type, body_buf)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer resp.Body.Close()
 
-  if err != nil {
-    panic(err.Error())
-    fmt.Println(resp.Status)
-  }else{
-    fmt.Println("Finished uploading..")
-    fmt.Println(resp.Status)
-  }
+	if err != nil {
+		panic(err.Error())
+		fmt.Println(resp.Status)
+	} else {
+		fmt.Println("Finished uploading..")
+		fmt.Println(resp.Status)
+	}
 
 }
